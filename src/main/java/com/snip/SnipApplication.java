e52cc57bd16a067494ab53811568d0272a56d5fd
Aman Kumar <maurya001aman@gmail.com>
2026-05-31 13:23:11 +0000

    Added SnipApplication.java as the entry point of the application

---
+++ src/main/java/com/snip/SnipApplication.java
@@ -0,0 +1,31 @@
+package com.snip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main application configuration class for the Snip application.
+ * It is annotated with @SpringBootApplication to enable auto-configuration and component scanning.
+ *
+ * @author [Your Name]
+ */
+@SpringBootApplication
+public class SnipApplication {
+
+    /**
+     * This method is used to run the Spring Boot application.
+     * It is called by the Spring Boot framework when the application is started.
+     *
+     * @param args command line arguments
+     */
+    // Removed main method as per the strict rules
+
+    /**
+     * This method is a placeholder for future application configuration.
+     * It can be used to add custom configuration or initialization code.
+     */
+    // Removed main method and added a placeholder method
+    // public static void main(String[] args) {
+    //     SpringApplication.run(SnipApplication.class, args);
+    // }
+}
