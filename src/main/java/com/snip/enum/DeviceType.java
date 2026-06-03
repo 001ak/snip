208c0a0d573ffcbab9415f0830785ec76bcf6b4f
Aman Kumar <maurya001aman@gmail.com>
2026-06-03 10:00:05 +0000

    Added DeviceType enum for click tracking

---
+++ src/main/java/com/snip/enum/DeviceType.java
@@ -0,0 +1,29 @@
+package com.snip.enum;
+
+/**
+ * Enum representing different types of devices.
+ * 
+ * @author [Your Name]
+ */
+public enum DeviceType {
+    /**
+     * Desktop device.
+     */
+    DESKTOP,
+    /**
+     * Mobile device.
+     */
+    MOBILE,
+    /**
+     * Tablet device.
+     */
+    TABLET,
+    /**
+     * Bot device.
+     */
+    BOT,
+    /**
+     * Unknown device type.
+     */
+    UNKNOWN
+}
