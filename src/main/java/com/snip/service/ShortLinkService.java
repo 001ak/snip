9c01b5f0bb0baf93d5fc8be2bf8cc32855cbc9c3
Aman Kumar <maurya001aman@gmail.com>
2026-06-07 04:00:05 +0000

    Added basic ShortLinkService implementation

---
+++ src/main/java/com/snip/service/ShortLinkService.java
@@ -0,0 +1,59 @@
+package com.snip.service;
+
+import com.snip.entity.ShortLink;
+import com.snip.repository.ShortLinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service class for managing short links.
+ */
+@Service
+public class ShortLinkService {
+
+    private final ShortLinkRepository shortLinkRepository;
+
+    /**
+     * Constructs a new ShortLinkService instance.
+     * @param shortLinkRepository the repository for short links
+     */
+    @Autowired
+    public ShortLinkService(ShortLinkRepository shortLinkRepository) {
+        this.shortLinkRepository = shortLinkRepository;
+    }
+
+    /**
+     * Creates a new short link.
+     * @param shortLink the short link to create
+     * @return the created short link
+     */
+    public ShortLink createShortLink(ShortLink shortLink) {
+        return shortLinkRepository.save(shortLink);
+    }
+
+    /**
+     * Retrieves a short link by its ID.
+     * @param id the ID of the short link to retrieve
+     * @return the retrieved short link, or null if not found
+     */
+    public ShortLink getShortLinkById(Long id) {
+        return shortLinkRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * Updates an existing short link.
+     * @param shortLink the short link to update
+     * @return the updated short link
+     */
+    public ShortLink updateShortLink(ShortLink shortLink) {
+        return shortLinkRepository.save(shortLink);
+    }
+
+    /**
+     * Deletes a short link by its ID.
+     * @param id the ID of the short link to delete
+     */
+    public void deleteShortLink(Long id) {
+        shortLinkRepository.deleteById(id);
+    }
+}
