57f18c60c6b844d77467435ed4f3a6a876464b5d
Aman Kumar <maurya001aman@gmail.com>
2026-06-24 15:00:06 +0000

    Enhance UrlValidator with reachability check

--- src/main/java/com/snip/util/UrlValidator.java
+++ src/main/java/com/snip/util/UrlValidator.java
@@ -2,6 +2,12 @@ package com.snip.util;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Utility class for validating URLs.
@@ -22,6 +28,31 @@ public class UrlValidator {
             URI uri = new URI(url);
             return uri.getScheme() != null && uri.getHost() != null;
         } catch (URISyntaxException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if a given URL is reachable by sending a HEAD request.
+     * 
+     * @param url the URL to be checked
+     * @return true if the URL is reachable, false otherwise
+     */
+    public boolean isUrlReachable(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                return false;
+            }
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+            }
+        });
+        try {
+            HttpStatus statusCode = restTemplate.headForHeaders(url).getStatusCode();
+            return statusCode.is2xxSuccessful();
+        } catch (Exception e) {
             return false;
         }
     }
