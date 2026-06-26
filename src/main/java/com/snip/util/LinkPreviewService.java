c0e839925d0b0a8eb6faf8b254d8f4c55cb6b409
Aman Kumar <maurya001aman@gmail.com>
2026-06-26 17:00:04 +0000

    Added LinkPreviewService to handle link preview functionality

---
+++ src/main/java/com/snip/util/LinkPreviewService.java
@@ -0,0 +1,33 @@
+package com.snip.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * Service class to fetch Open Graph metadata from a given URL.
+ */
+@Service
+public class LinkPreviewService {
+
+    /**
+     * Fetches the Open Graph title and description from a given URL.
+     * 
+     * @param url the URL to fetch metadata from
+     * @return a string array containing the title and description
+     * @throws IOException if an I/O error occurs
+     */
+    public String[] fetchOGMetadata(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Element titleElement = doc.selectFirst("meta[property='og:title']");
+        Element descriptionElement = doc.selectFirst("meta[property='og:description']");
+
+        String title = titleElement != null ? titleElement.attr("content") : null;
+        String description = descriptionElement != null ? descriptionElement.attr("content") : null;
+
+        return new String[] { title, description };
+    }
+}
